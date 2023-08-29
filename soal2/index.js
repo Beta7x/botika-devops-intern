7bb71ae11c5d5ced40a86b27d1a3e74f292ad4cc
Beta7x <adepriyantowidies@gmail.com>
2023-08-29 10:09:09 +0700

    Initial commit

---
+++ soal2/index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const os = require('os');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+    });
+    const response = JSON.stringify({
+        hostname: os.hostname(),
+        time: new Date().toLocaleString()
+    });
+    res.end(response);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is runnning on ${PORT}`);
+})
